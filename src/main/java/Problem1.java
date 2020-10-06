4545d0fbd252520b421569b4f7a4601bbc0147bf
SirAJn8r <hyperactive@live.com>
2020-10-05 18:52:37 -0700

    Fixed issue with Problem1 Added test cases to Problem1Test Updated Problem2Test to use assertArrayEquals

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,9 +1,13 @@
 public class Problem1
 {
-    // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target)
     {
-        int half = data.length / 2;
+        if(data == null || data.length == 0 || (data.length == 1 && data[0] != target))
+        {
+            return -1;
+        }
+
+        int half = (data.length - 1) / 2;
 
         if(target == data[half])
         {
@@ -11,17 +15,29 @@ public class Problem1
         }
         else if(target > data[half])
         {
-            int[] secondHalf = new int[data.length - half];
-            for(int i = 0; i < data.length - half; i++)
-                secondHalf[i] = data[half+i];
-            return half + binarySearch(secondHalf, target);
+            //secondHalf starts at last value - half
+            int[] secondHalf = new int[data.length - 1 - half];
+            for(int i = 0; i < secondHalf.length; i++)
+                //add everything past half
+                secondHalf[i] = data[half+i+1];
+
+            //add half + 1 (for half itself) to the returning index
+            //BUT if -1, return -1
+            int temp = binarySearch(secondHalf, target);
+            if(temp == -1) return -1;
+            else return half + 1 + temp;
         }
         else // target < data[half]
         {
             int[] firstHalf = new int[half];
             for(int i = 0; i < half; i++)
                 firstHalf[i] = data[i];
-            return binarySearch(data, target);
+
+            return binarySearch(firstHalf, target);
         }
+        /*
+         * Note: In both the else if and else statements,
+         * data[half] is not included in the next recursion as we know it is not the target
+         */
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -12,32 +12,28 @@ public class Problem2Test
         assertTrue(true);
 
         // Test if program loops enough times to fully reverse data set
-        int[] dataSet1 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        printArray(dataSet1);
-        Problem2.bubbleSort(dataSet1);
-        printArray(dataSet1);
-        System.out.println();
+        int[] input = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Problem2.bubbleSort(input);
+        assertArrayEquals(input, expected);
 
         // Test if program can deal with the far limits of integers, and test if it can work with arrays only 2 items
-        int[] dataSet2 = {Integer.MAX_VALUE, Integer.MIN_VALUE};
-        printArray(dataSet2);
-        Problem2.bubbleSort(dataSet2);
-        printArray(dataSet2);
-        System.out.println();
+        input = new int[] {Integer.MAX_VALUE, Integer.MIN_VALUE};
+        expected = new int[] {Integer.MIN_VALUE, Integer.MAX_VALUE};
+        Problem2.bubbleSort(input);
+        assertArrayEquals(input, expected);
 
         // Test if program can properly deal with the same number occurring multiple times, and negative numbers
-        int[] dataSet3 = {-20, 0, -10, 0, 3, 6, 6, 2, 6, 0, 10356};
-        printArray(dataSet3);
-        Problem2.bubbleSort(dataSet3);
-        printArray(dataSet3);
-        System.out.println();
+        input = new int[] {-20, 0, -10, 0, 3, 6, 6, 2, 6, 0, 10356};
+        expected = new int[] {-20, -10, 0, 0, 0, 2, 3, 6, 6, 6, 10356};
+        Problem2.bubbleSort(input);
+        assertArrayEquals(input, expected);
 
         // Test if program throws an error when given an array with only 1 item
-        int[] dataSet4 = {4};
-        printArray(dataSet4);
-        Problem2.bubbleSort(dataSet4);
-        printArray(dataSet4);
-        System.out.println();
+        input = new int[] {4};
+        expected = new int[] {4};
+        Problem2.bubbleSort(input);
+        assertArrayEquals(input, expected);
     }
 
     // Just a function to print the arrays in a nice way without repeating that code
